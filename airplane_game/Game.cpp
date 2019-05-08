70206f1687b1bfb2283e6b8b35121432de4584a4
GregWagner <theGreg101@gmail.com>
2019-05-08 12:49:04 -0500

    Completed chapter 2

--- airplane_game/Game.cpp
+++ airplane_game/Game.cpp
@@ -1,19 +1,44 @@
 #include "Game.h"
+#include <iostream>
+
+const float Game::PLAYER_SPEED { 100.0f };
+const sf::Time Game::TIME_PER_FRAME { sf::seconds(1.0f / 60.0f) };
 
 Game::Game()
-    : mWindow(sf::VideoMode(640, 480), "SFML Application")
+    : mWindow(sf::VideoMode(640, 480), "SFML Application", sf::Style::Close)
+    , mTexture()
     , mPlayer()
+    , mFont()
 {
-    mPlayer.setRadius(40.0f);
+    if (!mTexture.loadFromFile("Media/Textures/Eagle.png")) {
+        std::cerr << "Unable to load texture\n";
+    }
+    mPlayer.setTexture(mTexture);
     mPlayer.setPosition(100.0f, 100.0f);
-    mPlayer.setFillColor(sf::Color::Cyan);
+
+    mFont.loadFromFile("Media/Sansation.ttf");
+    mStatisticsText.setFont(mFont);
+    mStatisticsText.setPosition(5.0f, 5.0f);
+    mStatisticsText.setCharacterSize(10);
 }
 
 void Game::run()
 {
+    sf::Clock clock;
+    sf::Time timeSinceLastUpdate = sf::Time::Zero;
+
     while (mWindow.isOpen()) {
         processEvents();
-        update();
+
+        // always call update at a fixed time interval
+        sf::Time elapsedTime = clock.restart();
+        timeSinceLastUpdate += elapsedTime;
+        while (timeSinceLastUpdate > TIME_PER_FRAME) {
+            timeSinceLastUpdate -= TIME_PER_FRAME;
+            processEvents();
+            update(TIME_PER_FRAME);
+        }
+        updateStatistics(elapsedTime);
         render();
     }
 }
@@ -22,19 +47,68 @@ void Game::processEvents()
 {
     sf::Event event;
     while (mWindow.pollEvent(event)) {
-        if (event.type == sf::Event::Closed) {
+        switch (event.type) {
+        case sf::Event::KeyPressed:
+            handlePlayerInput(event.key.code, true);
+            break;
+        case sf::Event::KeyReleased:
+            handlePlayerInput(event.key.code, false);
+            break;
+        case sf::Event::Closed:
             mWindow.close();
+            break;
         }
     }
 }
 
-void Game::update()
+void Game::update(sf::Time deltaTime)
 {
+    sf::Vector2f movement(0.0f, 0.0f);
+    if (mIsMovingUp) {
+        movement.y -= PLAYER_SPEED;
+    }
+    if (mIsMovingDown) {
+        movement.y += PLAYER_SPEED;
+    }
+    if (mIsMovingRight) {
+        movement.x += PLAYER_SPEED;
+    }
+    if (mIsMovingLeft) {
+        movement.x -= PLAYER_SPEED;
+    }
+    mPlayer.move(movement * deltaTime.asSeconds());
 }
 
 void Game::render()
 {
     mWindow.clear();
     mWindow.draw(mPlayer);
+    mWindow.draw(mStatisticsText);
     mWindow.display();
+}
+
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+{
+    if (key == sf::Keyboard::W) {
+        mIsMovingUp = isPressed;
+    } else if (key == sf::Keyboard::S) {
+        mIsMovingDown = isPressed;
+    } else if (key == sf::Keyboard::A) {
+        mIsMovingLeft = isPressed;
+    } else if (key == sf::Keyboard::D) {
+        mIsMovingRight = isPressed;
+    }
+}
+
+void Game::updateStatistics(sf::Time elapsedTIme)
+{
+    mStatisticsUpdateTime += elapsedTIme;
+    ++mStatisticsNumberOfFrames;
+    if (mStatisticsUpdateTime >= sf::seconds(1.0f)) {
+        std::string s = "Frames / Second = " + std::to_string(mStatisticsNumberOfFrames) + '\n';
+        s += "Time / Update = " + std::to_string(mStatisticsUpdateTime.asMilliseconds() / mStatisticsNumberOfFrames) + "us";
+        mStatisticsText.setString(s);
+        mStatisticsUpdateTime -= sf::seconds(1.0f);
+        mStatisticsNumberOfFrames = 0;
+    }
 }
--- airplane_game/Game.h
+++ airplane_game/Game.h
@@ -2,16 +2,33 @@
 
 #include <SFML/Graphics.hpp>
 
-class Game {
+class Game : private sf::NonCopyable {
 public:
     Game();
     void run();
 
 private:
+    static const float PLAYER_SPEED;
+    static const sf::Time TIME_PER_FRAME;
+
     void processEvents();
-    void update();
+    void update(sf::Time deltaTime);
     void render();
 
+    void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
+    void updateStatistics(sf::Time elapsedTIme);
+
     sf::RenderWindow mWindow;
-    sf::CircleShape mPlayer;
+    sf::Texture mTexture;
+    sf::Sprite mPlayer;
+    sf::Font mFont;
+    sf::Text mStatisticsText {};
+    sf::Time mStatisticsUpdateTime {};
+
+    std::size_t mStatisticsNumberOfFrames;
+
+    bool mIsMovingUp {};
+    bool mIsMovingDown {};
+    bool mIsMovingRight {};
+    bool mIsMovingLeft {};
 };
